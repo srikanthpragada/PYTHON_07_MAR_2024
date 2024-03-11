8cb0f87de0d7a25d3f45e40b856c205ef1ec2cb0
srikanthpragada <srikanthpragada@gmail.com>
2024-03-11 19:55:57 +0530

    11-MAR

--- demo/circle_area.py
+++ demo/circle_area.py
@@ -3,7 +3,7 @@
 data = input("Enter radius :")
 radius = float(data)      # Convert str to float
 area = 3.14 * radius * radius
-print(area)
+print(f'Area of circle with radius {radius} is {area}')
 
 
 
